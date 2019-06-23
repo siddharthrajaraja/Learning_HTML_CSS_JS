087c5aad10eac6d7926d367183aa6e752f6a432c
SIDDHARTH RAMESH CHAND RAJA <45534008+siddharthrajaraja@users.noreply.github.com>
2019-06-23 09:39:42 +0530

    Arithematic Calculated made using Django Framework

---
+++ calculator/templates/page1.js
@@ -0,0 +1,28 @@
+
+function calc(op)
+           {
+             var  a=parseFloat(document.getElementById("text1").value,10);
+             var b=parseFloat(document.getElementById("text2").value,10);
+             let ans; 
+             let displaystring="Mod operator";
+
+             try{
+                 
+              switch(op)
+              {
+                  case '+': displaystr="Addition"; ans=a+b;break;
+                  case '-': displaystr="Substraction";ans=a-b;break;
+                  case '*':displaystr="Multiplication";ans=a*b;break;
+                  case '/':ans=a/b;break;
+
+              }  
+             document.getElementById("Lab1").innerHTML="Result of "+ displaystr +" is "+String(ans);
+                
+             }
+             catch(Exception)
+             {
+                   window.alert(Exception); 
+             }
+           
+           
+           }
